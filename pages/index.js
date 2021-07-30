ab53ea6e9f0481e7016d560cde4af6ac291904ef
wveit <wilbertveit@rocketmail.com>
2021-07-30 09:06:36 -0700

    add <title>

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { Provider } from 'react-redux';
 import store from '../state/store';
 import App from '../components/App';
+import Head from 'next/head';
 
 export default function Home() {
     return (
         <Provider store={store}>
+            <Head>
+                <title>Hitide</title>
+            </Head>
             <App />
         </Provider>
     );
