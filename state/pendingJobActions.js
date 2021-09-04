857a36a35f79b108bb355a231ebb7b70dcc926f0
wveit <wilbertveit@rocketmail.com>
2021-09-03 22:17:00 -0700

    Add all required data to pendingJob (in state)

--- state/pendingJobActions.js
+++ state/pendingJobActions.js
@@ -1,13 +1,37 @@
 import { doAddPendingJob as _doAddPendingJob } from './pendingJobSlice';
-import { selectCurrentGranuleFilter, selectSelectedGranules } from './granuleSearchSlice';
+import { selectDatasets } from './datasetSearchSlice';
+import { selectVariables } from './variablesSlice';
+import {
+    selectCurrentGranuleFilter,
+    selectSelectedGranules,
+    selectGranuleFilters,
+    selectGranules,
+} from './granuleSearchSlice';
 import { selectBbox } from './datasetSearchSlice';
 
 export const doAddPendingJob = () => (dispatch, getState) => {
     const id = new Date().getTime();
     const state = getState();
-    const datasetId = selectCurrentGranuleFilter(state);
-    const granules = Object.keys(selectSelectedGranules(state)[datasetId]);
-    const bbox = selectBbox(state);
 
-    dispatch(_doAddPendingJob({ id, datasetId, granules, bbox }));
+    const datasetId = selectCurrentGranuleFilter(state);
+    const datasetShortName = selectDatasets(state)[datasetId].shortName;
+
+    const granuleFilter = selectGranuleFilters(state)[datasetId];
+    const bbox = selectBbox(state);
+    const startDate = granuleFilter.startDate;
+    const endDate = granuleFilter.endDate;
+    const nameFilter = granuleFilter.name;
+
+    const allGranules = selectGranules(state)[datasetId];
+    const granuleIds = Object.keys(selectSelectedGranules(state)[datasetId]);
+    const granules = granuleIds.map((id) => allGranules[id].name);
+
+    const variables = {};
+    Object.values(selectVariables(state)[datasetId]).forEach((varName) => {
+        variables[varName] = true;
+    });
+
+    dispatch(
+        _doAddPendingJob({ id, datasetId, datasetShortName, granules, bbox, startDate, endDate, nameFilter, variables })
+    );
 };
