6f01f251f67822ecfe59b294db94cbd463738cb1
wveit <wilbertveit@rocketmail.com>
2021-09-24 17:25:21 -0700

    Improved UnsubmittedJobTable
    - Added job details with dropdown
    - Added actions for toggling variables

--- components/tables/UnsubmittedJobTable.js
+++ components/tables/UnsubmittedJobTable.js
@@ -1,15 +1,124 @@
 import { connect } from 'react-redux';
-import { selectPendingJobs } from '../../state/pendingJobSlice';
+import { selectPendingJobs, doToggleJobVariable, doRemovePendingJob } from '../../state/pendingJobSlice';
+import { useState } from 'react';
 
-function UnsubmittedJobTable({ pendingJobs }) {
+function Job({ job, onRemoveJob, onVariableToggle }) {
+    const [showDetails, setShowDetails] = useState(false);
+    const [showGranules, setShowGranules] = useState(false);
+    const [showVariables, setShowVariables] = useState(false);
+
+    return (
+        <div className='unsubmitted-job' key={job.id}>
+            <div className='main-row'>
+                <span className={'toggle' + (showDetails ? ' down' : '')} onClick={() => setShowDetails(!showDetails)}>
+                    &rsaquo;
+                </span>
+                <span>{job.datasetShortName}</span>
+                <span className='granule-count'>{job.granules.length} granules</span>
+                <span onClick={() => onRemoveJob(job.id)}>X</span>
+            </div>
+            <div className={'details-row' + (showDetails ? '' : ' hidden')}>
+                <div>Region: {job.bbox.join(', ')}</div>
+                <div>
+                    Date Range: {job.startDate ? new Date(job.startDate).toLocaleDateString() : '--'} to{' '}
+                    {job.endDate ? new Date(job.endDate).toLocaleDateString() : '--'}
+                </div>
+                <div>Granule Name Filter: {job.nameFilter || 'none'}</div>
+                <div>
+                    <span
+                        className={'toggle' + (showGranules ? ' down' : '')}
+                        onClick={() => setShowGranules(!showGranules)}
+                    >
+                        &rsaquo;
+                    </span>
+                    <span>Granules:</span>
+                </div>
+                <div className={showGranules ? '' : 'hidden'}>
+                    {job.granules.map((granule) => (
+                        <div key={granule}>
+                            <span>{granule}</span>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <span
+                        className={'toggle' + (showVariables ? ' down' : '')}
+                        onClick={() => setShowVariables(!showVariables)}
+                    >
+                        &rsaquo;
+                    </span>
+                    <span>Variables:</span>
+                </div>
+                <div className={showVariables ? '' : 'hidden'}>
+                    {Object.keys(job.variables).map((variable) => (
+                        <div key={variable}>
+                            <input
+                                type='checkbox'
+                                checked={job.variables[variable]}
+                                onChange={() => onVariableToggle({ jobId: job.id, variable })}
+                            />
+                            <span>{variable}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <style jsx>{`
+                .unsubmitted-job {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: stretch;
+                    width: 100%;
+                }
+
+                .main-row {
+                    display: flex;
+                    align-items: center;
+                    padding: 4px;
+                }
+
+                span {
+                    margin: 0 0.3rem;
+                }
+
+                .toggle {
+                    color: rgb(74, 131, 131);
+                    font-weight: bold;
+                    cursor: pointer;
+                    user-select: none;
+                }
+
+                .down {
+                    transform: rotate(90deg);
+                }
+
+                .granule-count {
+                    margin-left: auto;
+                }
+
+                .details-row {
+                    border-top: 1px solid lightgray;
+                }
+
+                .details-row > * {
+                    padding: 0.5rem 0.2rem 0;
+                }
+
+                .hidden {
+                    display: none;
+                }
+            `}</style>
+        </div>
+    );
+}
+
+function UnsubmittedJobTable({ pendingJobs, onRemoveJob, onVariableToggle }) {
     return (
         <div className='hitide-table'>
             <div className='hitide-table__content'>
                 {Object.values(pendingJobs).map((job) => (
-                    <div className='hitide-table__row' key={job.id}>
-                        <span>&gt;</span>
-                        <span>{job.datasetShortName}</span>
-                        <span>{job.granules.length} granules</span>
+                    <div key={job.id} className='hitide-table__row'>
+                        <Job job={job} onVariableToggle={onVariableToggle} onRemoveJob={onRemoveJob} />
                     </div>
                 ))}
             </div>
@@ -33,4 +142,9 @@ function select(state) {
     };
 }
 
-export default connect(select)(UnsubmittedJobTable);
+const actions = {
+    onRemoveJob: doRemovePendingJob,
+    onVariableToggle: doToggleJobVariable,
+};
+
+export default connect(select, actions)(UnsubmittedJobTable);
--- state/pendingJobSlice.js
+++ state/pendingJobSlice.js
@@ -15,10 +15,18 @@ const slice = createSlice({
         doRemoveAllPendingJobs(state, action) {
             state.pendingJobs = {};
         },
+        doRemovePendingJob(state, action) {
+            const jobId = action.payload;
+            delete state.pendingJobs[jobId];
+        },
+        doToggleJobVariable(state, action) {
+            const { jobId, variable } = action.payload;
+            state.pendingJobs[jobId].variables[variable] = !state.pendingJobs[jobId].variables[variable];
+        },
     },
 });
 
 export default slice.reducer;
-export const { doAddPendingJob, doRemoveAllPendingJobs } = slice.actions;
+export const { doAddPendingJob, doRemovePendingJob, doRemoveAllPendingJobs, doToggleJobVariable } = slice.actions;
 
 export const selectPendingJobs = (state) => state.pendingJob.pendingJobs;
