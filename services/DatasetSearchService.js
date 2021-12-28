b25f41418de9a15b28958aa9259a25f31b08ff1c
wveit <wilbertveit@rocketmail.com>
2021-12-28 00:55:25 -0800

    DatasetSearchService gets other params
    startDate, endDate, longName, description,
    imageUrl

--- services/DatasetSearchService.js
+++ services/DatasetSearchService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { DateNumber } from '../utils/DateNumber';
 
 export class DatasetSearchService {
     async fetchDatasets() {
@@ -11,6 +12,11 @@ export class DatasetSearchService {
             const dataset = {
                 id: rawDataset.id,
                 shortName: rawDataset.short_name,
+                startDate: rawDataset.time_start && DateNumber.fromIso(rawDataset.time_start),
+                endDate: rawDataset.time_end && DateNumber.fromIso(rawDataset.time_end),
+                longName: rawDataset.title,
+                description: rawDataset.summary,
+                imageUrl: 'https://podaac.jpl.nasa.gov/Podaac/thumbnails/' + rawDataset.short_name + '.jpg',
             };
             datasets[dataset.id] = dataset;
         });
