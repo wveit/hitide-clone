e945998147392d6df70a99cddb20a144b6449fc6
wveit <wilbertveit@rocketmail.com>
2022-01-03 21:30:17 -0800

    Fixed Drawer Toggle

--- components/App.js
+++ components/App.js
@@ -1,38 +1,46 @@
-import DatasetFilters from './DatasetFilters';
-import DatasetTable from './tables/DatasetTable';
-import GranuleSearchTable from './tables/GranuleSearchTable';
-import GranuleFilters from './GranuleFilters';
-import GranuleTable from './tables/GranuleTable';
-import GranuleSearchButtonBar from './GranuleSearchButtonBar';
-import UnsubmittedJobTable from './tables/UnsubmittedJobTable';
-import { Tabs } from './Tabs';
-import { Drawer } from './Drawer';
-import { useState } from 'react';
-import { TitleBar } from './TitleBar';
-import HitideMap from './HitideMap';
-import DatasetGranuleCounts from './DatasetGranuleCounts';
-import JobSubmitter from './JobSubmitter';
-import JobHistoryTable from './tables/JobHistoryTable';
-import DatasetInfoPage from './DatasetInfoPage';
+import DatasetFilters from "./DatasetFilters";
+import DatasetTable from "./tables/DatasetTable";
+import GranuleSearchTable from "./tables/GranuleSearchTable";
+import GranuleFilters from "./GranuleFilters";
+import GranuleTable from "./tables/GranuleTable";
+import GranuleSearchButtonBar from "./GranuleSearchButtonBar";
+import UnsubmittedJobTable from "./tables/UnsubmittedJobTable";
+import { Tabs } from "./Tabs";
+import { Drawer } from "./Drawer";
+import { useState } from "react";
+import { TitleBar } from "./TitleBar";
+import HitideMap from "./HitideMap";
+import DatasetGranuleCounts from "./DatasetGranuleCounts";
+import JobSubmitter from "./JobSubmitter";
+import JobHistoryTable from "./tables/JobHistoryTable";
+import DatasetInfoPage from "./DatasetInfoPage";
 
 export default function App() {
     const [drawerOpen, setDrawerOpen] = useState(true);
 
     return (
-        <div className='App'>
-            <div className='NonDrawerArea'>
+        <div className="App">
+            <div className="NonDrawerArea">
                 <HitideMap />
             </div>
-            <Drawer isOpen={drawerOpen} onToggle={() => setDrawerOpen(!drawerOpen)}>
+            <div
+                className="fas fa-bars DrawerButton"
+                onClick={() => setDrawerOpen(!drawerOpen)}
+            />
+            <Drawer isOpen={drawerOpen}>
                 <TitleBar onDrawerToggle={() => setDrawerOpen(!drawerOpen)} />
-                <Tabs tabs={['Search Datasets', 'Search Granules', 'Downloads']}>
+                <Tabs
+                    tabs={["Search Datasets", "Search Granules", "Downloads"]}
+                >
                     <section>
                         <h2>Dataset Search</h2>
                         <p>
-                            Search for datasets by date range, region, and parameter. Datasets selected in the results
-                            table will be available for subsetting and previewing in Granule Selection. Note: Dataset
-                            granules in Granule Selection will be constrained by the date range and region filters you
-                            apply below.
+                            Search for datasets by date range, region, and
+                            parameter. Datasets selected in the results table
+                            will be available for subsetting and previewing in
+                            Granule Selection. Note: Dataset granules in Granule
+                            Selection will be constrained by the date range and
+                            region filters you apply below.
                         </p>
                         <DatasetFilters />
                         <DatasetTable />
@@ -42,8 +50,9 @@ export default function App() {
                     <section>
                         <h2>GranuleSearch</h2>
                         <p>
-                            Download matching granules for all datasets, for individual datasets, or click on a dataset
-                            to further filter and preview its granules.{' '}
+                            Download matching granules for all datasets, for
+                            individual datasets, or click on a dataset to
+                            further filter and preview its granules.{" "}
                         </p>
                         <GranuleSearchTable />
                         <GranuleFilters />
@@ -53,8 +62,9 @@ export default function App() {
                     <section>
                         <h2>Downloads</h2>
                         <p>
-                            Review the selections you have made then submit them for processing. We will zip and email
-                            you the results.{' '}
+                            Review the selections you have made then submit them
+                            for processing. We will zip and email you the
+                            results.{" "}
                         </p>
                         <h4>Review Pending Jobs</h4>
                         <UnsubmittedJobTable />
@@ -95,6 +105,21 @@ export default function App() {
                 p {
                     color: gray;
                     font-style: italic;
+                }
+
+                .DrawerButton {
+                    position: absolute;
+                    top: 1rem;
+                    right: 1rem;
+                    background-color: rgb(233, 233, 233);
+                    border: 1px solid gray;
+                    border-radius: 4px;
+                    padding: 4px;
+                    cursor: pointer;
+                }
+
+                .DrawerButton:hover {
+                    color: rgb(66, 172, 175);
                 }
             `}</style>
         </div>
--- components/Drawer.js
+++ components/Drawer.js
@@ -1,11 +1,9 @@
-// import { DrawerToggle } from './Drawer';
-
-export function Drawer({ children, isOpen, onToggle, width = '600px' }) {
-    const closedClass = isOpen ? '' : ' closed';
+export function Drawer({ children, isOpen, width = "600px" }) {
+    const closedClass = isOpen ? "" : " closed";
     return (
-        <div className={'DrawerContainer' + closedClass}>
-            <DrawerToggle onClick={onToggle} />
-            <div className='DrawerContent'>{children}</div>
+        <div className={"DrawerContainer" + closedClass}>
+            <div className="DrawerContent">{children}</div>
+
             <style jsx>{`
                 .DrawerContainer {
                     position: relative;
@@ -20,41 +18,10 @@ export function Drawer({ children, isOpen, onToggle, width = '600px' }) {
                     width: 0px;
                 }
 
-                 {
-                    /* .DrawerToggle {
-                    position: absolute;
-                    right: 5px;
-                    top: 5px;
-                    width: 10px;
-                    height: 10px;
-                    background-color: red;
-                } */
-                }
-
                 .DrawerContent {
                     position: relative;
                     height: 100%;
-                    z-index: 100;
                     width: ${width};
-                }
-            `}</style>
-        </div>
-    );
-}
-
-function DrawerToggle({ onClick }) {
-    return (
-        <div className='fas fa-bars' onClick={onClick}>
-            <style jsx>{`
-                div {
-                    position: absolute;
-                    top: 0.5rem;
-                    right: 0.5rem;
-                    cursor: pointer;
-                    font-size: 1.3rem;
-                }
-                div:hover {
-                    color: rgb(116, 155, 206);
                 }
             `}</style>
         </div>
