a11c4c3e9f1a7aa3c213febed682bfc2387211fc
Wilbert Veit <wilbert.e.veit@jpl.nasa.gov>
2022-01-04 22:58:14 -0800

    GranuleSearch dates can be changed

--- components/GranuleFilters.js
+++ components/GranuleFilters.js
@@ -1,77 +1,147 @@
-import { connect } from 'react-redux';
-import { selectCurrentGranuleFilter } from '../state/granuleSearchSlice';
-import { selectDatasetColors } from '../state/datasetSearchSlice';
+import { useState } from "react";
 
-function GranuleFilters({ datasetId, datasetColors }) {
-    return (
-        <div className='container'>
-            <h3>{datasetId}</h3>
-            <div className='row row1'>
-                <div className='label'>Filter By Name:</div>
-                <input type='text' id='granule-name-filter' />
-            </div>
-            <div className='row row2'>
-                <div className='label'>Filter By Date:</div>
-                <span>From</span>
-                <input type='date' id='granule-startDate-filter' value='2000-01-01' readOnly />
-                <span>To</span>
-                <input
-                    type='date'
-                    id='granule-endDate-filter'
-                    value={new Date().toISOString().split('T')[0]}
-                    readOnly
-                />
-            </div>
+import { connect } from "react-redux";
+import {
+  selectGranuleFilters,
+  selectCurrentGranuleFilter,
+  doSetStartDate,
+  doSetEndDate,
+} from "../state/granuleSearchSlice";
+import { selectDatasetColors } from "../state/datasetSearchSlice";
+import { DateNumber } from "../utils/DateNumber";
 
-            <style jsx>{`
-                .container {
-                    padding: 2rem 0;
-                }
+function GranuleFilters({
+  datasetId,
+  datasetColors,
+  startDate,
+  endDate,
+  onNewStartDate,
+  onNewEndDate,
+}) {
+  const start = startDate ? format(new Date(startDate)) : "";
+  const end = endDate ? format(new Date(endDate)) : "";
 
-                h3 {
-                    margin: 0;
-                    margin-bottom: 0.5rem;
-                    border-bottom: 2px solid ${datasetColors[datasetId]};
-                    display: inline-block;
-                }
+  function handleStartDateChange(event) {
+    const dateString = event.target.value;
+    if (!dateString) {
+      onNewStartDate({ datasetId, startDate: null });
+    } else {
+      const dateNumber = DateNumber.fromIso(dateString);
+      onNewStartDate({ datasetId, startDate: dateNumber });
+    }
+  }
 
-                .row {
-                    width: 80%;
-                    display: grid;
-                    justify-items: center;
-                    align-items: center;
-                    padding: 0.2rem 0;
-                }
+  function handleEndDateChange(event) {
+    console.log(event.target.value);
+    const dateString = event.target.value;
+    if (!dateString) {
+      onNewEndDate({ datasetId, endDate: null });
+    } else {
+      const dateNumber = DateNumber.fromIso(dateString);
+      onNewEndDate({ datasetId, endDate: dateNumber });
+    }
+  }
 
-                .row1 {
-                    grid-template-columns: 7rem 1fr;
-                }
+  return (
+    <div className="container">
+      <h3>{datasetId}</h3>
+      <div className="row date-row">
+        <div className="label">Filter By Date:</div>
+        <span>From</span>
+        <input
+          type="date"
+          id="granule-startDate-filter"
+          value={start}
+          onChange={handleStartDateChange}
+        />
+        <span>To</span>
+        <input
+          type="date"
+          id="granule-endDate-filter"
+          value={end}
+          onChange={handleEndDateChange}
+        />
+      </div>
 
-                .row2 {
-                    grid-template-columns: 7rem auto 1fr auto 1fr;
-                }
+      <style jsx>{`
+        .container {
+          padding: 2rem 0;
+        }
 
-                .row > * {
-                    margin-right: 1.2rem;
-                }
+        h3 {
+          margin: 0;
+          margin-bottom: 0.5rem;
+          border-bottom: 2px solid ${datasetColors[datasetId]};
+          display: inline-block;
+        }
 
-                input {
-                    width: 100%;
-                }
+        .row {
+          width: 80%;
+          display: grid;
+          justify-items: center;
+          align-items: center;
+          padding: 0.2rem 0;
+        }
 
-                .label {
-                    justify-self: left;
-                }
-            `}</style>
-        </div>
-    );
+        .date-row {
+          grid-template-columns: 7rem auto 1fr auto 1fr;
+        }
+
+        .row > * {
+          margin-right: 1.2rem;
+        }
+
+        input {
+          width: 100%;
+        }
+
+        .label {
+          justify-self: left;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-function select(state) {
-    return {
-        datasetId: selectCurrentGranuleFilter(state),
-        datasetColors: selectDatasetColors(state),
-    };
+function mapStateToProps(state) {
+  const datasetId = selectCurrentGranuleFilter(state);
+  const currentFilter = selectGranuleFilters(state)[datasetId];
+  const startDate = currentFilter.startDate;
+  const endDate = currentFilter.endDate;
+
+  return {
+    datasetId,
+    startDate,
+    endDate,
+    datasetColors: selectDatasetColors(state),
+  };
 }
 
-export default connect(select)(GranuleFilters);
+const mapDispatchToProps = {
+  onNewStartDate: doSetStartDate,
+  onNewEndDate: doSetEndDate,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GranuleFilters);
+
+function format(date, defaultDate) {
+  if (!date) {
+    date = defaultDate;
+  }
+  const newDate = new Date(date);
+
+  const yearString = newDate.getUTCFullYear();
+  const month = newDate.getUTCMonth() + 1;
+  let monthString = "" + month;
+  if (month < 10) {
+    monthString = "0" + month;
+  }
+  let dayOfMonth = newDate.getUTCDate();
+  let dayOfMonthString = "" + dayOfMonth;
+  if (dayOfMonth < 10) {
+    dayOfMonthString = "0" + dayOfMonth;
+  }
+
+  const dateString = `${yearString}-${monthString}-${dayOfMonthString}`;
+  return dateString;
+}
--- state/granuleSearchSlice.js
+++ state/granuleSearchSlice.js
@@ -1,134 +1,147 @@
-import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from "@reduxjs/toolkit";
 
 const initialState = {
-    granules: {},
-    filters: {},
-    selectedGranules: {},
-    footprintGranules: {},
-    previewGranules: {},
-    currentFilter: null,
+  granules: {},
+  filters: {},
+  selectedGranules: {},
+  footprintGranules: {},
+  previewGranules: {},
+  currentFilter: null,
 };
 
 const slice = createSlice({
-    name: 'granuleSearch',
-    initialState,
-    reducers: {
-        doCreateGranuleFilter(state, action) {
-            const datasetId = action.payload;
-            const filter = {
-                datasetId,
-                startDate: null,
-                endDate: null,
-                name: null,
-            };
-            state.filters[datasetId] = filter;
-            state.currentFilter = datasetId;
-            state.granules[datasetId] = {};
-            state.selectedGranules[datasetId] = {};
-            state.footprintGranules[datasetId] = {};
-            state.previewGranules[datasetId] = {};
-        },
-        doDeleteGranuleFilter(state, action) {
-            const datasetId = action.payload;
-            delete state.filters[datasetId];
-            delete state.granules[datasetId];
-            delete state.selectedGranules[datasetId];
-            delete state.footprintGranules[datasetId];
-            delete state.previewGranules[datasetId];
-            const filterArray = Object.keys(state.filters);
-            if (filterArray.length == 0) {
-                state.currentFilter = null;
-            } else if (state.currentFilter === datasetId) {
-                state.currentFilter = filterArray[0];
-            }
-        },
-        doDeleteAllGranuleFilters(state) {
-            state.filters = {};
-            state.currentFilter = null;
-            state.granules = {};
-            state.selectedGranules = {};
-            state.footprintGranules = {};
-            state.previewGranules = {};
-        },
-        doSetCurrentGranuleFilter(state, action) {
-            state.currentFilter = action.payload;
-        },
-        doAddGranules(state, action) {
-            const { datasetId, granules } = action.payload;
-            state.granules[datasetId] = { ...state.granules[datasetId], ...granules };
-        },
-        doSetSelectedGranueles(state, action) {
-            const { datasetId, selectedGranules } = action.payload;
-            state.selectedGranules[datasetId] = selectedGranules;
-        },
-        doAddFootprintGranule(state, action) {
-            const { datasetId, granuleId } = action.payload;
-            state.footprintGranules[datasetId][granuleId] = true;
-        },
-        doRemoveFootprintGranule(state, action) {
-            const { datasetId, granuleId } = action.payload;
-            delete state.footprintGranules[datasetId][granuleId];
-        },
-        doAddPreviewGranule(state, action) {
-            const { datasetId, granuleId } = action.payload;
-            state.previewGranules[datasetId][granuleId] = true;
-        },
-        doRemovePreviewGranule(state, action) {
-            const { datasetId, granuleId } = action.payload;
-            delete state.previewGranules[datasetId][granuleId];
-        },
+  name: "granuleSearch",
+  initialState,
+  reducers: {
+    doCreateGranuleFilter(state, action) {
+      const datasetId = action.payload;
+      const filter = {
+        datasetId,
+        startDate: null,
+        endDate: null,
+      };
+      state.filters[datasetId] = filter;
+      state.currentFilter = datasetId;
+      state.granules[datasetId] = {};
+      state.selectedGranules[datasetId] = {};
+      state.footprintGranules[datasetId] = {};
+      state.previewGranules[datasetId] = {};
     },
+    doDeleteGranuleFilter(state, action) {
+      const datasetId = action.payload;
+      delete state.filters[datasetId];
+      delete state.granules[datasetId];
+      delete state.selectedGranules[datasetId];
+      delete state.footprintGranules[datasetId];
+      delete state.previewGranules[datasetId];
+      const filterArray = Object.keys(state.filters);
+      if (filterArray.length == 0) {
+        state.currentFilter = null;
+      } else if (state.currentFilter === datasetId) {
+        state.currentFilter = filterArray[0];
+      }
+    },
+    doDeleteAllGranuleFilters(state) {
+      state.filters = {};
+      state.currentFilter = null;
+      state.granules = {};
+      state.selectedGranules = {};
+      state.footprintGranules = {};
+      state.previewGranules = {};
+    },
+    doSetCurrentGranuleFilter(state, action) {
+      state.currentFilter = action.payload;
+    },
+    doAddGranules(state, action) {
+      const { datasetId, granules } = action.payload;
+      state.granules[datasetId] = { ...state.granules[datasetId], ...granules };
+    },
+    doSetSelectedGranueles(state, action) {
+      const { datasetId, selectedGranules } = action.payload;
+      state.selectedGranules[datasetId] = selectedGranules;
+    },
+    doAddFootprintGranule(state, action) {
+      const { datasetId, granuleId } = action.payload;
+      state.footprintGranules[datasetId][granuleId] = true;
+    },
+    doRemoveFootprintGranule(state, action) {
+      const { datasetId, granuleId } = action.payload;
+      delete state.footprintGranules[datasetId][granuleId];
+    },
+    doAddPreviewGranule(state, action) {
+      const { datasetId, granuleId } = action.payload;
+      state.previewGranules[datasetId][granuleId] = true;
+    },
+    doRemovePreviewGranule(state, action) {
+      const { datasetId, granuleId } = action.payload;
+      delete state.previewGranules[datasetId][granuleId];
+    },
+    doSetStartDate(state, action) {
+      const { datasetId, startDate } = action.payload;
+      state.filters[datasetId].startDate = startDate;
+    },
+    doSetEndDate(state, action) {
+      const { datasetId, endDate } = action.payload;
+      state.filters[datasetId].endDate = endDate;
+    },
+  },
 });
 
 export default slice.reducer;
 export const {
-    doCreateGranuleFilter,
-    doDeleteGranuleFilter,
-    doDeleteAllGranuleFilters,
-    doSetCurrentGranuleFilter,
-    doAddGranules,
-    doSetSelectedGranueles,
-    doAddFootprintGranule,
-    doRemoveFootprintGranule,
-    doAddPreviewGranule,
-    doRemovePreviewGranule,
+  doCreateGranuleFilter,
+  doDeleteGranuleFilter,
+  doDeleteAllGranuleFilters,
+  doSetCurrentGranuleFilter,
+  doAddGranules,
+  doSetSelectedGranueles,
+  doAddFootprintGranule,
+  doRemoveFootprintGranule,
+  doAddPreviewGranule,
+  doRemovePreviewGranule,
+  doSetStartDate,
+  doSetEndDate,
 } = slice.actions;
 
 export const selectGranuleFilters = (state) => state.granuleSearch.filters;
-export const selectCurrentGranuleFilter = (state) => state.granuleSearch.currentFilter;
+export const selectCurrentGranuleFilter = (state) =>
+  state.granuleSearch.currentFilter;
 export const selectGranules = (state) => state.granuleSearch.granules;
-export const selectSelectedGranules = (state) => state.granuleSearch.selectedGranules;
-export const selectFootprintGranuleIds = (state) => state.granuleSearch.footprintGranules;
-export const selectPreviewGranuleIds = (state) => state.granuleSearch.previewGranules;
+export const selectSelectedGranules = (state) =>
+  state.granuleSearch.selectedGranules;
+export const selectFootprintGranuleIds = (state) =>
+  state.granuleSearch.footprintGranules;
+export const selectPreviewGranuleIds = (state) =>
+  state.granuleSearch.previewGranules;
 
 export const selectFootprintGranuleList = createSelector(
-    selectFootprintGranuleIds,
-    selectGranules,
-    (footprintGranuleIds, granules) => {
-        const footprintGranules = [];
+  selectFootprintGranuleIds,
+  selectGranules,
+  (footprintGranuleIds, granules) => {
+    const footprintGranules = [];
 
-        Object.keys(footprintGranuleIds).forEach((datasetId) => {
-            Object.keys(footprintGranuleIds[datasetId]).forEach((granuleId) => {
-                footprintGranules.push(granules[datasetId][granuleId]);
-            });
-        });
+    Object.keys(footprintGranuleIds).forEach((datasetId) => {
+      Object.keys(footprintGranuleIds[datasetId]).forEach((granuleId) => {
+        footprintGranules.push(granules[datasetId][granuleId]);
+      });
+    });
 
-        return footprintGranules;
-    }
+    return footprintGranules;
+  }
 );
 
 export const selectPreviewGranuleList = createSelector(
-    selectPreviewGranuleIds,
-    selectGranules,
-    (previewGranuleIds, granules) => {
-        const previewGranules = [];
+  selectPreviewGranuleIds,
+  selectGranules,
+  (previewGranuleIds, granules) => {
+    const previewGranules = [];
 
-        Object.keys(previewGranuleIds).forEach((datasetId) => {
-            Object.keys(previewGranuleIds[datasetId]).forEach((granuleId) => {
-                previewGranules.push(granules[datasetId][granuleId]);
-            });
-        });
+    Object.keys(previewGranuleIds).forEach((datasetId) => {
+      Object.keys(previewGranuleIds[datasetId]).forEach((granuleId) => {
+        previewGranules.push(granules[datasetId][granuleId]);
+      });
+    });
 
-        return previewGranules;
-    }
+    return previewGranules;
+  }
 );
